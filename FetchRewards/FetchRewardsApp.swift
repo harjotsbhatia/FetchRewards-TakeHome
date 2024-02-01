520b0488ea190fdf052a3d10fc7bd5dcb207a058
harjotsbhatia <89289963+harjotsbhatia@users.noreply.github.com>
2024-01-31 16:15:34 -0800

    Initial Commit

---
+++ FetchRewards/FetchRewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsApp.swift
+//  FetchRewards
+//
+//  Created by Harjot Bhatia on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
